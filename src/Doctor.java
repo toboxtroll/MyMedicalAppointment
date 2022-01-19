e886625cea8b1cfe91b74becb51856adc43c9507
toboxtroll <tobiazz012@gmail.com>
2022-01-18 21:42:23 -0500

    Creando la primera clase

---
+++ src/Doctor.java
@@ -0,0 +1,9 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    public void showName(){
+        System.out.println(name);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
      public static void main(String[] args) {
-         System.out.println("Hola mundo");
+//         System.out.println("Hola mundo");
+         Doctor myDoctor = new Doctor();
+         myDoctor.name = "Tobias Sandoval";
+         myDoctor.showName();
     }
 }

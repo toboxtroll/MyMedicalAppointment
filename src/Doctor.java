ec35ca17502c69a718df05b88725213a794b27b2
toboxtroll <tobiazz012@gmail.com>
2022-01-18 22:23:15 -0500

    Definiendo método constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,13 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del doctor asignado es: "+name);
+    }
     public void showName(){
         System.out.println(name);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
      public static void main(String[] args) {
 //         System.out.println("Hola mundo");
-         Doctor myDoctor = new Doctor();
+         Doctor myDoctor = new Doctor("Nicolás Sandoval");
+         myDoctor.showName();
          myDoctor.name = "Tobias Sandoval";
          myDoctor.showName();
     }

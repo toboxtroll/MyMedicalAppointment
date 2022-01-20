c4bd2679e34f7c6de9e63187a321d930fa1971d3
toboxtroll <tobiazz012@gmail.com>
2022-01-19 22:22:50 -0500

    Se modifica el metodo constructor para iniciar el objeto doctor con el nombre y la especialidad

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,15 +1,16 @@
 public class Doctor {
-    static int id;
+    static int id = 0;
     String name;
     String speciality;
 
     Doctor(){
-        System.out.println("Construyendo el objeto Doctor");
-        id ++;
+
     }
 
-    Doctor(String name){
+    Doctor(String name, String speciality){
         System.out.println("El nombre del doctor asignado es: "+name);
+        this.name = name;
+        this.speciality = speciality;
     }
     public void showName(){
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import static ui.UIMenu.*;
 
 public class Main {
      public static void main(String[] args) {
+         Doctor myDoctor = new Doctor("Tobias Sandoval", "Pediatra");
+         System.out.println(myDoctor.name);
+         System.out.println(myDoctor.speciality);
          showMenu();
     }
 }

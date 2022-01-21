18b3acc7a4683fc0cab701fc37bbb50de43fb281
toboxtroll <tobiazz012@gmail.com>
2022-01-21 07:32:10 -0500

    Se reduce el nivel del scoop de las variables de la clase patient y se crean los metodos getters y setters para acceder y asignar datos

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,15 @@ public class Main {
          System.out.println(myDoctor.speciality);
 
          Patient myPatient = new Patient("Nico", "nico@gmail.com");
-         System.out.println(myPatient.name);
-         System.out.println(myPatient.email);
+
+         myPatient.setHeight(164.5);
+         System.out.println(myPatient.getHeight());
+
+         myPatient.setWeight(66.7);
+         System.out.println(myPatient.getWeight());
+
+         myPatient.setPhoneNumber("12345689");
+         System.out.println(myPatient.getPhoneNumber());
 //         showMenu();
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -1,16 +1,85 @@
 public class Patient {
-    String name;
-    String email;
-    String address;
-    String phoneNumber;
-    String birthday;
-    double weight;
-    double height;
-    String blood;
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
 
     Patient(String name, String email){
         System.out.println("Se crea el obejeto paciente");
         this.name = name;
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber.length() > 8){
+            System.out.println("El número telefónico debe ser máximo de 8 dígitos");
+        }else if (phoneNumber.length() == 8) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getWeight() {
+        return this.weight + "Kg.";
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return this.height + "Cm.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
     }
 }
